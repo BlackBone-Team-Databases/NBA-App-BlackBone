57f197a307206ec526fb3150a5eb564fa437f761
purlantov <ppurlantov@yahoo.com>
2017-06-03 14:04:28 +0300

    Initial App

---
+++ TeamWorkTEST/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TeamWorkTEST.Models
+{
+    public class Team
+    {
+        public Team()
+        {
+
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+		public string Url { get; set; }
+
+	}
+}
---
+++ TeamWorkTEST/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWorkTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NbaContext())
+            {
+                db.Players.Add(new Models.Player { Name = "Pavel Ivanov" });
+                db.SaveChanges();
+            }
+        }
+    }
+}
